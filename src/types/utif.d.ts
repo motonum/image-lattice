8aefb58a156fdfdfd03c6053879cc8a4578160c7
motonum <73626946+motonum@users.noreply.github.com>
2025-11-02 01:11:03 +0900

    fix: 型エラーの解消

---
+++ src/types/utif.d.ts
@@ -0,0 +1,15 @@
+declare module 'utif' {
+  type IFD = any
+
+  export function decode(buffer: ArrayBuffer | Uint8Array): IFD[]
+  export function decodeImages(buffer: ArrayBuffer | Uint8Array, ifds: IFD[]): void
+  export function toRGBA8(ifd: IFD): Uint8Array
+
+  const UTIF: {
+    decode: typeof decode
+    decodeImages: typeof decodeImages
+    toRGBA8: typeof toRGBA8
+  }
+
+  export default UTIF
+}
