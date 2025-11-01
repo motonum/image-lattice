1773901e2f91afa288d8768423d11f5bbd517626
motonum <73626946+motonum@users.noreply.github.com>
2025-11-02 04:39:24 +0900

    chore: 軽微な修正

--- src/components/CanvasRenderer.tsx
+++ src/components/CanvasRenderer.tsx
@@ -243,7 +243,7 @@ const CanvasRenderer = forwardRef<CanvasHandle | null, Props>(
 
 				{/* Preview area: visible when preview prop is true. We scale via CSS so large canvases fit in the UI. */}
 				<div
-					className="mt-5 block max-w-[1000px]"
+					className="mt-5 block max-w-[960px] mx-auto"
 					style={{ display: preview ? "block" : "none" }}
 				>
 					<canvas
